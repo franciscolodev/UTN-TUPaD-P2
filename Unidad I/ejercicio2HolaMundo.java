dd2c0240aaa5c19fdf90f7e1bdbb3834ef084199
Francisco López <152048893+franciscolodev@users.noreply.github.com>
2025-08-15 20:28:28 -0300

    Add files via upload

---
+++ Unidad I/ejercicio2HolaMundo.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+/*
+ * Ejercicio 2 TP1 Programación II - Francisco López (Comisión 3)
+ */
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+       
+        System.out.println("¡Hola, Java!");
+    }
+    
+}
+
