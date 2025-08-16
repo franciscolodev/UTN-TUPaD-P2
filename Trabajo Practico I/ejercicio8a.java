9dd117a60d0da7ed79fa77b9a54c1ab9bc6178d7
Francisco López <152048893+franciscolodev@users.noreply.github.com>
2025-08-15 23:15:22 -0300

    Update ejercicio8a.java

--- Trabajo Practico I/ejercicio8a.java
+++ Trabajo Practico I/ejercicio8a.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Ejercicio8a {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // cambiamos 'sc' por 'input'
+        Scanner input = new Scanner(System.in); 
 
         System.out.print("Ingrese el primer número: ");
-        int num1 = input.nextInt(); // usamos 'input' aquí
+        int num1 = input.nextInt(); 
 
         System.out.print("Ingrese el segundo número: ");
         int num2 = input.nextInt();
@@ -14,6 +14,7 @@ public class Ejercicio8a {
         int resultado = num1 / num2;
         System.out.println("Resultado (int): " + resultado);
 
-        input.close(); // cerramos 'input'
+        input.close(); 
     }
 }
+
