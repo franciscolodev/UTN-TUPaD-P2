aebd5d8c455137c9c91a7840e4e11fbfe1025ea1
Francisco López <152048893+franciscolodev@users.noreply.github.com>
2025-08-15 22:18:52 -0300

    Update and rename Ejercicio5.java to ejercicio5.java

--- Trabajo Practico I/ejercicio5.java
+++ Trabajo Practico I/ejercicio5.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio5;
 import java.util.Scanner;
 
@@ -16,13 +12,11 @@ public class Ejercicio5 {
         System.out.print("Ingrese el segundo número entero: ");
         int numero2 = Integer.parseInt(input.nextLine());
 
-        // Operaciones
         int suma = numero1 + numero2;
         int resta = numero1 - numero2;
         int multiplicacion = numero1 * numero2;
         double division = (double) numero1 / numero2;
 
-        // Resultados
         System.out.println("Suma: " + suma);
         System.out.println("Resta: " + resta);
         System.out.println("Multiplicación: " + multiplicacion);
